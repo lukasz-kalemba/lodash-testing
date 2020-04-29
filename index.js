8d77a955e75b26b6213bf4a4efe82cf662d89ea9
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2020-04-29 19:01:05 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const _ = require('lodash');
+
+//! chunk
+// const array = [2, 5, 7, 4, 2, 1, 8, 9, 4, 0, 7];
+// const newArray = _.chunk(array, [(_.size = 2)]);
+
+//! compact
+// const array = [2, '', 7, NaN, false, 2, true, 0, 7];
+// const newArray = _.compact(array);
+
+//! concat
+//const array = [1, 2, 3];
+//const newArray = _.concat(array, [97, 98, 99]);
+
+//! diffference
+// const array = [1, 2, 3];
+// const newArray = _.difference(array, [2, 2, 2]);
+
+//! diffference
+// const array = [1, 2, 3];
+// const newArray = _.difference(array, [2, 2, 2]);
+
+//! max
+// const array = [9, 10, 21, 99, 100];
+// const newArray = _.max(array);
+
+//! min
+// const array = [9, 10, 21, 99, 100];
+// const newArray = _.min(array);
+
+//! toArray
+// const newArray = _.toArray('qwerty');
+
+//! sortBy
+// var users = [
+//   { user: 'fred', age: 48 },
+//   { user: 'barney', age: 36 },
+//   { user: 'fred', age: 40 },
+//   { user: 'barney', age: 34 },
+//   { user: 'adam', age: 63 },
+// ];
+
+// const newArray = _.sortBy(users, [
+//   function (obj) {
+//     return obj.age;
+//   },
+// ]);
+
+// var array = [4, 7, 10, 12, 35, 1, 4, 120, 3, 67, 23, 1231];
+// const newArray = _.sortBy(array);
+
+//! shuffle
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const newArray = _.shuffle(array);
+
+console.log(newArray);
